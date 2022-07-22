c77bf8916b5df5d250dffc792922729f25e9fb42
AndreyGarzon <36746939+AndreyGarzon@users.noreply.github.com>
2022-07-21 20:38:17 -0500

    Update UserAccess API Group

--- app/Http/Requests/UpdateUserAccessRequest.php
+++ app/Http/Requests/UpdateUserAccessRequest.php
@@ -27,7 +27,7 @@ class UpdateUserAccessRequest extends FormRequest
             'role_id'=>'required|integer',
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255',
             'password' => 'required|string|min:8',
         ];
     }
