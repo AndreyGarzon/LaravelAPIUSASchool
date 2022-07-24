af06c86aecbe175b7dda4ba8c4e2178a762a9290
AndreyGarzon <36746939+AndreyGarzon@users.noreply.github.com>
2022-07-23 22:20:58 -0500

    Update api logout

--- routes/api.php
+++ routes/api.php
@@ -8,29 +8,14 @@ use App\Http\Controllers\GameResultController;
 use App\Http\Controllers\GroupController;
 use App\Http\Controllers\SessionGameController;
 use App\Http\Controllers\StudentController;
-use App\Models\GameResult;
-use App\Models\SessionGame;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 
 
 // Auth API
 
 Route::post('/user/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('/logout',[AuthController::class,'logout']);
 // Route::post('/password/email', 'Api\ForgotPasswordController@sendResetLinkEmail');
 // Route::post('/password/reset', 'Api\ResetPasswordController@reset');
 Route::get('/email/resend', [VerificationController::class,'resend'])->name('verification.resend');
