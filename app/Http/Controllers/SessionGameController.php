54772835809eba2b46295cd9794162907653124b
Andrey Garzon <andreygarzonquiroga@gmail.com>
2022-08-21 19:02:55 -0500

    Update Apis

--- app/Http/Controllers/SessionGameController.php
+++ app/Http/Controllers/SessionGameController.php
@@ -62,7 +62,9 @@ class SessionGameController extends Controller
                                         ->join('groups','teachers.id','=','groups.teacher_id')
                                         ->join('students','groups.id','=','students.group_id')
                                         ->join('session_games','students.id','=','session_games.student_id')
-                                        ->join('game_groups','session_games.game_group_id','=','game_groups.id')
+                                        ->join('game_results','session_games.id','=','game_results.session_game_id')
+                                        ->join('games','game_results.game_id','=','games.id')
+                                        ->join('game_groups','games.game_group_id','=','game_groups.id')
                                         ->where('session_games.state_session_game_id','2')->where('users.id',$id)->distinct()->get();
         
         return $session_games;
